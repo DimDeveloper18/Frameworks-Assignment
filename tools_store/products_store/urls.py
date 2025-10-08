92ab18819cd460891d50f184154c871bb5e99329
DimDeveloper18 <dimash.itcout@gmail.com>
2025-10-08 18:09:51 +0100

    File 'urls.py' path's created in 'nav' section for (tools_page, contact_page, delivery_page) and in header section for (register_page, login_page, basket_page, logout).

--- tools_store/products_store/urls.py
+++ tools_store/products_store/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='products_store-index'),
+    path('', views.index, name='products_store-tools_page'),
+    path('', views.index, name='products_store-contact_page'),
+    path('', views.index, name='products_store-delivery_page'),
+    path('', views.index, name='products_store-register_page'),
+    path('', views.index, name='products_store-login_page'),
+    path('', views.index, name='products_store-basket_page'),
+    path('', views.index, name='products_store-logout'),
 ]
