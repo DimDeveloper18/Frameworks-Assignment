4b673a4b8e0267f218236b54c1b8ba7d95bd3907
DimDeveloper18 <dimash.itcout@gmail.com>
2025-10-12 01:27:09 +0100

    File 'views.py' function created for tools page.

--- tools_store/products_store/views.py
+++ tools_store/products_store/views.py
@@ -7,3 +7,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'products_store/index.html')
+
+def tools(request):
+    return render(request, 'products_store/tools.html')
