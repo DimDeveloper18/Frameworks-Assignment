21565c4f1a801534055fc9d0612022e299e3bc2e
DimDeveloper18 <dimash.itcout@gmail.com>
2025-10-13 15:19:30 +0100

    File 'urls.py' path name changed to 'contact' from 'contact_page'.

--- tools_store/products_store/urls.py
+++ tools_store/products_store/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='products_store-index'),
     path('tools/', views.tools, name='products_store-tools'),
-    path('', views.index, name='products_store-contact_page'),
+    path('contact/', views.contact, name='products_store-contact'),
     path('', views.index, name='products_store-delivery_page'),
     path('', views.index, name='products_store-register_page'),
     path('', views.index, name='products_store-login_page'),
