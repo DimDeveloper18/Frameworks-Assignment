a596727d4f85176b77ef0c270600f79fa93ac02a
DimDeveloper18 <dimash.itcout@gmail.com>
2025-10-27 16:34:01 +0000

    File 'views.py' in 'comments' app changed: django librarie for comment added and function 'comments_view' created.

--- tools_store/custom_comments/views.py
+++ tools_store/custom_comments/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Comment
+
 
 # Create your views here.
+
+
+def comments_view(request):
+    com_consist = {
+        'comments': Comment.objects.all()
+    }
+    return render(request, 'custom_comments/comments_page.html', com_consist)
+
