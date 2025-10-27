4653b58f3c4445bf09f07c9b2db0c9aa10d8acbc
DimDeveloper18 <dimash.itcout@gmail.com>
2025-10-27 16:48:31 +0000

    File 'apps.py' in 'comments' app changed: class name, and app name, as sintax error was found.

--- tools_store/custom_comments/apps.py
+++ tools_store/custom_comments/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CastomCommentsConfig(AppConfig):
+class CustomCommentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'castom_comments'
+    name = 'custom_comments'
