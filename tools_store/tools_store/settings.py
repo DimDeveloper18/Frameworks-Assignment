9efb52e454b687e8d405595cc83863aa812f44ce
DimDeveloper18 <dimash.itcout@gmail.com>
2025-10-27 16:58:27 +0000

    File 'settings.py' app 'custom_comments' added to apps.

--- tools_store/tools_store/settings.py
+++ tools_store/tools_store/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'tools_store',
     'products_store',
+    'custom_comments',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
