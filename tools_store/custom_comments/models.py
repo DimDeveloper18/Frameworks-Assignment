826a9f54521ad4dc4a88de578f255056653a3772
DimDeveloper18 <dimash.itcout@gmail.com>
2025-10-29 18:40:02 +0000

    File 'models.py' in 'comments' app changed: syntax refinements.

--- tools_store/custom_comments/models.py
+++ tools_store/custom_comments/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth import get_user_model
-User = get_user_model
+User = get_user_model()
 
 # Create your models here.
 
@@ -9,9 +9,9 @@ class Comment(models.Model):
     comwriter = models.ForeignKey(User, on_delete=models.CASCADE)
     comname = models.CharField(max_length=100)
     comtext = models.TextField()
-    commented_date = models.DateTimeField(defult=timezone.now)
+    commented_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.title
+        return self.comname
     
     
