7387f3a57db9d723fd1cd52306db7c200dcf50ab
DimDeveloper18 <dimash.itcout@gmail.com>
2025-10-31 16:01:37 +0000

    File 'views.py' in 'customers' app changed: function created for 'reg_form' and django library added for 'UserCreationForm'.

--- tools_store/customers/views.py
+++ tools_store/customers/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
+
+def reg_form(request):
+    form = UserCreationForm()
+    return render(request, 'customers/register.html', {'form':form})
+
