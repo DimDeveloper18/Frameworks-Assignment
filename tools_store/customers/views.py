c7c041f5d005a57f89c1b5e85e14d0a8ce7d22f0
DimDeveloper18 <dimash.itcout@gmail.com>
2025-11-01 23:44:45 +0000

    File 'views.py' in 'customers' app changed: 'if else' statement created for form validation and messages. Django library's added for 'UserRegisterForm' and  'messages'.

--- tools_store/customers/views.py
+++ tools_store/customers/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
-
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from django.contrib import messages
 
 # Create your views here.
 
 def reg_form(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Wellcome {'username'}!')
+            return redirect('products_store-index')
+    else:
+        form = UserRegisterForm()
+
     return render(request, 'customers/register.html', {'form':form})
 
