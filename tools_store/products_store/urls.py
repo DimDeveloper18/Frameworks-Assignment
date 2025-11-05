f7a97e85c3ec8aa5fdf6ab8c0057769e33131f7e
DimDeveloper18 <dimash.itcout@gmail.com>
2025-11-05 18:16:10 +0000

    File 'urls.py' in 'products_store' app changed: temporary code deleted.

--- tools_store/products_store/urls.py
+++ tools_store/products_store/urls.py
@@ -7,8 +7,4 @@ urlpatterns = [
     path('tools/', views.tools, name='products_store-tools'),
     path('contact/', views.contact, name='products_store-contact'),
     path('delivery/', views.delivery, name='products_store-delivery'),
-    path('', views.index, name='products_store-register_page'),
-    path('', views.index, name='products_store-login_page'),
-    path('', views.index, name='products_store-basket_page'),
-    path('', views.index, name='products_store-logout'),
 ]
