84e60c13051ec667fbe4fef6c43ba92c16e1f0e1
DimDeveloper18 <dimash.itcout@gmail.com>
2025-11-06 11:12:43 +0000

    File 'urls.py' in 'tools_store' app changed: path logout value in 'name' element changed for 'customers-logout'.

--- tools_store/tools_store/urls.py
+++ tools_store/tools_store/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('', include('custom_comments.urls')),
     path('register/', customers_views.reg_form, name='customers-reg_form'),
     path('login/', auth_views.LoginView.as_view(template_name='customers/login.html'), name='customers-login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='customers/logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='customers/logout.html'), name='customers-logout'),
 ]
