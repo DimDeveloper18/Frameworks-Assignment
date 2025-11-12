c916655050ed1c765c9196ec2bf987d27d80d8bb
DimDeveloper18 <dimash.itcout@gmail.com>
2025-11-12 18:12:13 +0000

    Django libraries added for 'settings' and 'static', then if statement crteated for 'settings'.

--- tools_store/tools_store/urls.py
+++ tools_store/tools_store/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from customers import views as customers_views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,3 +31,6 @@ urlpatterns = [
     path('basket/', customers_views.basket_page, name='customers-basket_page'),
     path('profile/', customers_views.profile_page, name='customers-profile_page'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
