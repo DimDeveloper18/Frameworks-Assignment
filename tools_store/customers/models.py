ea0b6f57d873df80e20f206ed6098d56ede13e1f
DimDeveloper18 <dimash.itcout@gmail.com>
2025-11-13 14:44:41 +0000

    Function 'save' created for save and  auto resize profile image. Django library added for 'Image'.

--- tools_store/customers/models.py
+++ tools_store/customers/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 # Create your models here.
@@ -10,4 +11,14 @@ class User_profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} User_profile'
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        prof_img = Image.open(self.image.path)
+
+        if prof_img.height > 70 or prof_img.width > 70:
+            resize = (70, 70)
+            prof_img.thumbnail(resize)
+            prof_img.save(self.image.path)
 
